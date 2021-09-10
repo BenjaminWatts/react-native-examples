e8dbf18ded8278ffa28807cbab6c160cd942c295
Ben Watts <ben@kilowatts.io>
2021-09-10 12:18:23 +0100

    add build for web

--- examples/web/Readme.md
+++ examples/web/Readme.md
@@ -1,3 +1,5 @@
 # React Native Web
 
 This example can be run both in the browser (using react-native-web) and on ios/android.
+
+All code must be prettified, otherwise the web build will through an error.
--- examples/web/src/App.tsx
+++ examples/web/src/App.tsx
@@ -3,7 +3,13 @@ import {View, Text} from 'react-native';
 
 export const App = () => {
   return (
-    <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100%'}}>
+    <View
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100%',
+      }}>
       <Text>All in one app</Text>
     </View>
   );
