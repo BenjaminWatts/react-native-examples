538c9d0932c2f657eb2ed96996314c92024d17da
Ben Watts <ben@kilowatts.io>
2021-09-10 12:02:33 +0100

    add react native web

---
+++ examples/web/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const App = () => {
+  return (
+    <View>
+      <Text>All in one app</Text>
+    </View>
+  );
+};
+
+export default App;
